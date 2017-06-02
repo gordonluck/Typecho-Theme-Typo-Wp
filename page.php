27828f7ce0ab8923dc9bf4d2bdf6919077bbb628
闫芝明 <yanzhiming520@live.com>
2017-06-02 22:10:25 +0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
+          <div class="content">
+            <h1><?php $this->title() ?></h1>
+            <a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a>&nbsp;&nbsp;
+            <?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>&nbsp;&nbsp;
+            <?php $this->content(); ?>
+            </div>
+          <div class="content">
+<?php include('comments.php'); ?>
+            </div>
+</div>
+<?php $this->need('sidebar.php'); ?>
+<?php $this->need('footer.php'); ?>
